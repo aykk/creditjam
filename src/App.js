bf37384d142699342142e673641e93479a73d04d
ayk <jungmink623@gmail.com>
2024-09-14 02:57:35 -0400

    remake app in ts

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import { AppRoutes } from './AppRoutes';
-import React from 'react';
-
-export const App = () => {
-  return (
-    <AppRoutes />
-
-  );
-}
--- src/pages/FormPages.js
+++
@@ -1,8 +0,0 @@
-export const FormPages = () => {
-    return (
-        <>
-        <h1>Credit Card Form</h1>
-        <p>Please complete the form to get a recommended card!</p>
-        </>
-    );
-}
