37c8a1ad565c2e9701f40c4816aa851d6a96faca
ayk <jungmink623@gmail.com>
2024-09-15 05:10:53 -0400

    complete page tweaked

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6e82849636ce86e1"; }
+/******/ 		__webpack_require__.h = function() { return "85b48cf4427763b1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
